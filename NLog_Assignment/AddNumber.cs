dbe079d93ba8ee70dd88567af1c17f8a5ecf9e83
Prerana <dahiwadeprerana@gmail.com>
2022-12-01 15:39:13 +0530

    [prerana]Add:Nlog

---
+++ NLog_Assignment/AddNumber.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLog_Assignment
+{
+    public class AddNumber
+    {
+        private readonly NLog nLog = new NLog();
+        public int Sum(int firstNumber, int secondNumber)
+        {
+            if (firstNumber == 0 || secondNumber == 0)
+            {
+                nLog.LogDebug("Debug Successfull : Sum()");
+                nLog.LogError("Expecting null values");
+                nLog.LogWarn("secondNumber or firstNumber should not equal to 0");
+            }
+            int result = secondNumber + firstNumber;
+            nLog.LogDebug("Debug Successfull : Sum()");
+            nLog.LogInfo("Sum Method passed, Result: " + result);
+            return result;
+        }
+    }
+}
+    
---
+++ NLog_Assignment/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace NLog_Assignment
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            int a =0;
+            int b = 1;
+            Console.WriteLine("Addition Problem \n");
+            AddNumber add = new AddNumber();
+            Console.WriteLine("Result is " + " " + add.Sum(a, b));
+            
+        }
+    }
+}
